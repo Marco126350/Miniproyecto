46a841d6ff2fa223d308dfb46d65dfa0af445c5d
Marco126350 <89754904+Marco126350@users.noreply.github.com>
2021-12-04 22:19:16 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js");
+
 if (workbox) { 
 	console.Log("Ajua! Workbox esta cargado! :) "); 
 	workbox.precaching.precacheAndRoute([]); 
+
 	/*Cache de imagenes en la carpeta, por ejemplo "others-, editamos a otras carpetas que se obtuvieron y configuramos en el archivo sw config.js */ 
 	workbox.routing.registerRoute( 
 		/(.*)others(.*)\.(?:png|gif|jpg)/, 
-		new workbox. strategies.CacheFirst({ 
+		new workbox.strategies.CacheFirst({ 
 			cacheName: "images",
 			plugins:[
 				new workbox.expiration.Plugin({
 					maxEntries: 50,
-					maxAgeseconds: 30 * 24 * 60 * 60,
+					maxAgeSeconds: 30 * 24 * 60 * 60,
 				})
 
 			] 
@@ -25,11 +27,11 @@ if (workbox) {
 	//Usamos el nombre de un cache personalizado. 
 	cacheName: "assets", 
 }) 
-) ; 
+); 
 //Cache de fuentes de Google 
 workbox.routing.registerRoute( 
 new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"), 
-new workbox. strategies.CacheFirst({ 
+new workbox.strategies.CacheFirst({ 
 	cacheName: "google-fonts",
 	plugins: [
 	new workbox.cacheableResponse.Plugin({
@@ -38,11 +40,14 @@ new workbox. strategies.CacheFirst({
 	],
 	}) 
 );
+
 //agregar analisis offline
 workbox.googleAnalytics.initialize();
+
 /*instalar un nuevo service worker y hacer que actualice y controle la pagina web lo antes posible*/
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
+
 } else {
 	console.log("¡Fallo! workbox no esta cargado ): ");
 }
